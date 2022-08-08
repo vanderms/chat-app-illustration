200bd6cb73d20ba39b853ddce1f69f1ba2dcc708
vanderms <vanderms.84@gmail.com>
2022-08-08 18:43:05 -0300

    feat: implement responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import { MobileIllustration } from "./components/mobile";
 function App() {
   return (
     <main className="bg-patterns">
-      <div className="container relative z-10 py-16 grid grid-cols-1 gap-16">
+      <div
+        className="container relative z-10 py-16 grid grid-cols-1 gap-16 
+        xl:grid-cols-[15.4375rem,27.8125rem] xl:justify-between xl:py-[9.25rem]"
+      >
         <MobileIllustration />
-        <Header />
+        <div className="self-center">
+          <Header />
+        </div>
       </div>
     </main>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 export const Header: React.FC = () => {
   return (
     <header>
-      <h1 className="headline-xl text-neutral-800 text-center">Simple booking</h1>
-      <p>
+      <h1 className="headline-xl text-neutral-800 text-center xl:text-left">Simple booking</h1>
+      <p className="mt-6 body-md text-neutral-500 text-center xl:text-left ">
         Stay in touch with our dog walkers through the chat interface. This makes it easy to
         discuss arrangements and make bookings. Once the walk has been completed you can
         rate your walker and book again all through the chat.
@@ -10,4 +10,3 @@ export const Header: React.FC = () => {
     </header>
   );
 };
-
